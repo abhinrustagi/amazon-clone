3e37853ac0e3b95880c8660b40bdf4ebf2a7f544
abhinrustagi <rustagi.abhin@gmail.com>
2021-03-03 12:19:01 +0530

    -

--- passport/initializePassport.js
+++ passport/initializePassport.js
@@ -4,6 +4,12 @@ const cryptojs = require("crypto-js");
 // const getUserByEmail = require("./getUser");
 const getUserByID = require("./getUserById");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+const JWTStrategy = require("passport-jwt").Strategy,
+  ExtractJWT = require("passport-jwt").ExtractJwt;
+
+require("dotenv").config({ path: "../" });
 
 function initialize(passport) {
   const authenticateUser = async (email, password, done) => {
@@ -27,7 +33,33 @@ function initialize(passport) {
     });
   };
 
-  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
+  const opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme("JWT"),
+    secret: process.env.JWT_SECRET,
+  };
+
+  passport.use(
+    "jwt",
+    new JWTStrategy(opts, (jwt_payload, done) => {
+      User.findOne({ email: jwt_payload.email }, (err, user) => {
+        if (err) {
+          done(null, false);
+        } else {
+          if (user) {
+            console.log(user);
+          }
+        }
+      });
+    })
+  );
+
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email", passwordField: "password", session: "false" },
+      authenticateUser
+    )
+  );
+
   passport.serializeUser((user, done) => done(null, user));
   passport.deserializeUser((_id, done) => {
     return done(null, getUserByID(_id));
